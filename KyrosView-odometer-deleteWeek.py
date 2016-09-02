1e11265d797da1b3a516fd9e53095ed2406dbf99
Carlos Rueda Morales <crueda@gmail.com>
2016-09-02 10:32:24 +0200

    minor changes

---
+++ KyrosView-odometer-deleteWeek.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+# autor: Carlos Rueda
+# date: 2016-09-02
+# version: 1.0
+
+##################################################################################
+# version 1.0 release notes: extract data from MySQL and generate json
+# Initial version
+# Requisites: library python-mysqldb. To install: "apt-get install python-mysqldb"
+##################################################################################
+
+import logging, logging.handlers
+import os
+import json
+import sys
+import datetime
+import calendar
+import time
+import MySQLdb
+
+#### VARIABLES #########################################################
+from configobj import ConfigObj
+config = ConfigObj('./KyrosView-backend.properties')
+
+JSON_DIR = config['directory_jsons']
+LOG_FILE = config['directory_logs'] + "/kyrosView-odometer.log"
+LOG_FOR_ROTATE = 10
+
+DB_IP = config['BBDD_host']
+DB_PORT = config['BBDD_port']
+DB_NAME = config['BBDD_name']
+DB_USER = config['BBDD_username']
+DB_PASSWORD = config['BBDD_password']
+
+########################################################################
+# definimos los logs internos que usaremos para comprobar errores
+try:
+	logger = logging.getLogger('kyrosView-odometer')
+	loggerHandler = logging.handlers.TimedRotatingFileHandler(LOG_FILE , 'midnight', 1, backupCount=10)
+	formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+	loggerHandler.setFormatter(formatter)
+	logger.addHandler(loggerHandler)
+	logger.setLevel(logging.DEBUG)
+except:
+	print '------------------------------------------------------------------'
+	print '[ERROR] Error writing log at %s' % LOG_FILE
+	print '[ERROR] Please verify path folder exits and write permissions'
+	print '------------------------------------------------------------------'
+	exit()
+########################################################################
+
+def deleteOdometerWeek():
+	dbConnection = MySQLdb.connect(DB_IP, DB_USER, DB_PASSWORD, DB_NAME)
+	try:
+		dbConnection = MySQLdb.connect(DB_IP, DB_USER, DB_PASSWORD, DB_NAME)
+	except:
+		logger.error('Error connecting to database: IP:%s, USER:%s, PASSWORD:%s, DB:%s: %s', DB_IP, DB_USER, DB_PASSWORD, DB_NAME, error)
+
+	cursor = dbConnection.cursor()
+	query = """UPDATE ODOMETER set N_WEEK=0,  
+		WEEK_SPEED_AVERAGE=0, WEEK_DISTANCE=0, WEEK_CONSUME=0"""
+	cursor.execute(query)
+	dbConnection.commit()
+	cursor.close
+	dbConnection.close
+		
+deleteOdometerWeek()
